7d220a85c318dbd7b756e6a2ea93fee6a611bd3f
Xtreme44 <vini.fsm9@live.com>
2022-08-28 22:06:40 -0300

    ChangesV1
    -Correção de alguns erros
    -Includes funcionando
    -Algumas funções do empréstimo

--- SegundoTrabalhoAED.cpp
+++ SegundoTrabalhoAED.cpp
@@ -47,7 +47,7 @@ struct IdxLivros{
 	int end;
 };
 
-struct Emprestimo{
+struct Emprestimos{
 	int cod;
 	int codRa;
 	int codLivro;
@@ -61,7 +61,7 @@ struct Emprestimo{
 	char statusEmp[1];
 };
 
-struct IdxEmprestimo{
+struct IdxEmprestimos{
 	int cod;
 	int end;
 };
@@ -71,9 +71,71 @@ void mainMenu (){
 	cout << "\n\nMenu";
 	cout << "\n0 - Sair";
 	cout << "\n1 - Alunos";
-	cout << "\n2 - Editoras";
-	cout << "\n3 - Livros";
+	cout << "\n2 - Livros";
+	cout << "\n3 - Editoras";
 	cout << "\n4 - Empréstimos";
+}
+
+string buscarEditoras (struct Editoras e1[], struct IdxEditoras i1[], int cont, int cod){
+	int i = 0, f = cont, x = 0;
+    int m = (i + f) / 2;
+    while (x == 0){
+    for (; f >= i && cod != i1[m].cod; m = (i + f) / 2){
+        if (cod > i1[m].cod)
+            i = m + 1;
+        else
+            f = m - 1;
+    }
+    i = i1[m].end;
+    if ((cod == i1[m].cod)){
+		return(e1[i].nome);
+    }
+    else{
+    	return("Não encontrado");
+	}
+	}
+}
+
+string buscarAlunos (struct Alunos a1[], struct IdxAlunos i1[], int cont, int cod){
+	int i = 0, f = cont, x = 0;
+    int m = (i + f) / 2;
+    while (x == 0){
+    for (; f >= i && cod != i1[m].cod; m = (i + f) / 2){
+        if (cod > i1[m].cod)
+            i = m + 1;
+        else
+            f = m - 1;
+    }
+    i = i1[m].end;
+    if ((cod == i1[m].cod)){
+		return(a1[i].nome);
+    }
+    else{
+    	return("Não encontrado");
+	}
+	}
+}
+
+string buscarLivros (struct Livros l1[], struct IdxLivros i1[], int cont, int cod){
+	int i = 0, f = cont, x = 0;
+    int m = (i + f) / 2;
+    while (x == 0){
+    for (; f >= i && cod != i1[m].cod; m = (i + f) / 2){
+        if (cod > i1[m].cod)
+            i = m + 1;
+        else
+            f = m - 1;
+    }
+    i = i1[m].end;
+    if ((cod == i1[m].cod)){
+    	strcpy(l1[i].statusEmp,"E");
+		return(l1[i].desc);
+    }
+    else{
+    	cout << "\nNão encontrado. Digite Novamente: ";
+    	cin >> cod;;
+	}
+	}
 }
 
 void leituraIdxAlunos (struct IdxAlunos idx[], int cont){
@@ -110,6 +172,15 @@ void leituraAlunos (struct Alunos a1[], struct IdxAlunos i1[], int &cont){
     leituraIdxAlunos(i1,cont);
 }
 
+void leituraIdxEditoras (struct IdxEditoras idx[], int cont){
+    for (int i = 0; i < cont; i++){
+        cout << "\n\nCodigo do Indice da Editora " << (i+1) << ": ";
+        cin >> idx[i].cod;
+        cout << "Endereco Fisico na Area de Dados: ";
+        cin >> idx[i].end;
+    }
+}
+
 void leituraEditoras (struct Editoras e1[], struct IdxEditoras i1[], int &cont){
     int i = 0;
     for (int saida = 1; i < 20 && saida != 0; i++){
@@ -126,34 +197,42 @@ void leituraEditoras (struct Editoras e1[], struct IdxEditoras i1[], int &cont){
             cin >> e1[i].cidade;
             cout << "Estado: ";
             cin >> e1[i].uf;
-			i1[i].cod = e1[i].cod;
-			i1[i].end = i;
+
 			e1[i].status = 0;
         }
         else saida = 0;
     }
     cont = i-1;
+    leituraIdxEditoras(i1,cont);
 }
 
-void leituraLivros (struct Livros l1[], struct IdxLivros i1[], int &cont){
+void leituraIdxLivros (struct IdxLivros idx[], int cont){
+    for (int i = 0; i < cont; i++){
+        cout << "\n\nCodigo do Indice do Livro " << (i+1) << ": ";
+        cin >> idx[i].cod;
+        cout << "Endereco Fisico na Area de Dados: ";
+        cin >> idx[i].end;
+    }
+}
+
+void leituraLivros (struct Livros l1[], struct IdxLivros i1[], int &cont, struct Editoras e1[],struct IdxEditoras ie1[], int contEdit){
     int i = 0;
     for (int saida = 1; i < 20 && saida != 0; i++){
-        cout << "\n\nCodigo da Editora " << (i+1) << ": ";
+        cout << "\n\nCodigo do Livro " << (i+1) << ": ";
         cin >> l1[i].cod;
         if (l1[i].cod > 0){
             cout << "Descrição: ";
             cin >> l1[i].desc;
             cout << "Codigo da Editora: ";
             cin >> l1[i].codEdit;
-            cout << "Situação: ";
-            cin >> l1[i].status;
-			i1[i].cod = l1[i].cod;
-			i1[i].end = i;
+    		cout << "Nome da Editora: " << buscarEditoras(e1,ie1, contEdit, l1[i].codEdit) << endl;
+			strcpy(l1[i].statusEmp,"D");
 			l1[i].status = 0;
         }
         else saida = 0;
     }
     cont = i-1;
+    leituraIdxLivros(i1,cont);
 }
 
 void incluirAlunos (struct Alunos a1[], struct IdxAlunos i1[], int &cont, int cod){
@@ -166,7 +245,7 @@ void incluirAlunos (struct Alunos a1[], struct IdxAlunos i1[], int &cont, int co
         else
             f = m - 1;
     }
-    i = i1[m].end;
+
     if ((cod == i1[m].cod)){
         cout << "\n\n Aluno já existe";
         cout << "RA: ";
@@ -176,7 +255,7 @@ void incluirAlunos (struct Alunos a1[], struct IdxAlunos i1[], int &cont, int co
     	x = 1 ;
 	}
 }
-    cont++;
+
 	a1[cont].ra = cod;
     cout << "Nome: ";
     cin >> a1[cont].nome;
@@ -190,13 +269,21 @@ void incluirAlunos (struct Alunos a1[], struct IdxAlunos i1[], int &cont, int co
     cin >> a1[cont].uf;
     a1[cont].status = 0;
     // inclusao na area de indices
-    for (i = cont - 1; i1[i].cod > cod; i--){
+    if(cont == 0){
+	i1[0].cod = cod;
+    i1[0].end = cont;
+	}
+	else{
+    for (i = cont - 1; i1[i].cod > cod && i >= 0; i--){
         i1[i+1].cod = i1[i].cod;
         i1[i+1].end = i1[i].end;
     }
     i1[i+1].cod = cod;
     i1[i+1].end = cont;
+}
     cout << "\n\nInclusao realizada com Sucesso";
+    cont++;
+    getch();
 }
 
 void incluirEditoras (struct Editoras e1[], struct IdxEditoras i1[], int &cont, int cod){
@@ -209,17 +296,17 @@ void incluirEditoras (struct Editoras e1[], struct IdxEditoras i1[], int &cont, 
         else
             f = m - 1;
     }
-    i = i1[m].end;
+
     if ((cod == i1[m].cod)){
         cout << "\n\n Editora já existe";
-        cout << "\nCódigo: ";
+        cout << "Código: ";
     	cin >> cod;
     }
     else{
     	x = 1 ;
 	}
-	}
-    cont++;
+}
+
 	e1[cont].cod = cod;
     cout << "Nome: ";
     cin >> e1[cont].nome;
@@ -231,17 +318,26 @@ void incluirEditoras (struct Editoras e1[], struct IdxEditoras i1[], int &cont, 
     cin >> e1[cont].cidade;
     cout << "Estado: ";
     cin >> e1[cont].uf;
-    e1[i].status = 0;
-    for (i = cont - 1; i1[i].cod > cod; i--){
+    e1[cont].status = 0;
+    // inclusao na area de indices
+    if(cont == 0){
+	i1[0].cod = cod;
+    i1[0].end = cont;
+	}
+	else{
+    for (i = cont - 1; i1[i].cod > cod && i >= 0; i--){
         i1[i+1].cod = i1[i].cod;
         i1[i+1].end = i1[i].end;
     }
     i1[i+1].cod = cod;
     i1[i+1].end = cont;
+}
     cout << "\n\nInclusao realizada com Sucesso";
+    cont++;
+    getch();
 }
 
-void incluirLivros (struct Livros l1[], struct IdxLivros i1[], int &cont, int cod){
+void incluirLivros (struct Livros l1[], struct IdxLivros i1[], int &cont, int cod, struct Editoras e1[],struct IdxEditoras ie1[], int contEdit){
 	int i = 0, f = cont, x = 0;
     int m = (i + f) / 2;
     while (x == 0){
@@ -251,7 +347,6 @@ void incluirLivros (struct Livros l1[], struct IdxLivros i1[], int &cont, int co
         else
             f = m - 1;
     }
-    i = i1[m].end;
     if ((cod == i1[m].cod)){
         cout << "\n\n Livro já existe";
         cout << "Código: ";
@@ -260,23 +355,33 @@ void incluirLivros (struct Livros l1[], struct IdxLivros i1[], int &cont, int co
     else{
     	x = 1 ;
 	}
-	}
-    cont++;
+}
+    // inclusao na area de indices
+
 	l1[cont].cod = cod;
     cout << "Descricao: ";
     cin >> l1[cont].desc;
     cout << "Codigo da Editora: ";
     cin >> l1[cont].codEdit;
-    cout << "Status: ";
-    cin >> l1[cont].status;
-    l1[i].status = 0;
-    for (i = cont - 1; i1[i].cod > cod; i--){
+    cout << "Nome da Editora: " << buscarEditoras(e1,ie1, contEdit, l1[i].codEdit) << endl;
+	strcpy(l1[cont].statusEmp,"D");
+    l1[cont].status = 0;
+    
+    if(cont == 0){
+	i1[0].cod = cod;
+    i1[0].end = cont;
+	}
+	else{
+    for (i = cont - 1; i1[i].cod > cod && i >= 0; i--){
         i1[i+1].cod = i1[i].cod;
         i1[i+1].end = i1[i].end;
     }
     i1[i+1].cod = cod;
     i1[i+1].end = cont;
+}
     cout << "\n\nInclusao realizada com Sucesso";
+    cont++;
+    getch();
 }
 
 void excluirLivros (struct IdxLivros idx[], struct Livros l1[], int &cont, int cod){
@@ -342,15 +447,138 @@ void mostrarAlunos(struct Alunos a1[], struct IdxAlunos i1[], int cont){
 	for(int i=0;i<cont;i++){
 		m=i1[i].end;
 		if(a1[m].status==0){
-		cout << "RA:" << a1[m].ra << endl;
+		cout << "\nRA:" << a1[m].ra << endl;
 		cout << "Nome:" << a1[m].nome << endl;
-		cout << "Endereço" << a1[m].endereco << endl;
-		cout << "Telefone" << a1[m].telefone << endl;
-		cout << "Cidade" << a1[m].cidade << endl;
+		cout << "Endereço:" << a1[m].endereco << endl;
+		cout << "Telefone:" << a1[m].telefone << endl;
+		cout << "Cidade:" << a1[m].cidade << endl;
 		cout << "Estado:" << a1[m].uf << endl;		
 	}
 }
 getch();
+}
+
+void mostrarLivros(struct Livros l1[], struct IdxLivros i1[], int cont, struct Editoras e1[],struct IdxEditoras ie1[], int contEdit){
+	int m;
+	cout << "\n\nLivros" << endl;
+	for(int i=0;i<cont;i++){
+		m=i1[i].end;
+		if(l1[m].status==0){
+		cout << "\nCódigo:" << l1[m].cod << endl;
+		cout << "Descrição:" << l1[m].desc << endl;
+		cout << "Editora:" << buscarEditoras(e1,ie1,contEdit,l1[m].codEdit) << endl;
+		cout << "Situação:" << l1[m].statusEmp << endl;	
+	}
+}
+getch();
+}
+
+void mostrarEditoras(struct Editoras e1[], struct IdxEditoras i1[], int cont){
+	int m;
+	cout << "\n\nEditoras" << endl;
+	for(int i=0;i<cont;i++){
+		m=i1[i].end;
+		if(e1[m].status==0){
+		cout << "\nCódigo:" << e1[m].cod << endl;
+		cout << "Nome:" << e1[m].nome << endl;
+		cout << "Endereço:" << e1[m].endereco << endl;
+		cout << "Telefone:" << e1[m].telefone << endl;
+		cout << "Cidade:" << e1[m].cidade << endl;
+		cout << "Estado:" << e1[m].uf << endl;		
+	}
+}
+getch();
+}
+
+void emprestarLivros(struct Emprestimos e1[],struct IdxEmprestimos i1[], int &cont, int cod, struct Livros l1[], struct IdxLivros il1[], int contL, struct Alunos a1[], struct IdxAlunos ia1[], int contA){
+	int codLivro, codAluno;
+	int i = 0, f = cont, x = 0;
+    int m = (i + f) / 2;
+    while (x == 0){
+    for (; f >= i && cod != i1[m].cod; m = (i + f) / 2){
+        if (cod > i1[m].cod)
+            i = m + 1;
+        else
+            f = m - 1;
+    }
+    if ((cod == i1[m].cod)){
+        cout << "\n\n Empréstimo já existe";
+        cout << "Código: ";
+    	cin >> cod;
+    }
+    else{
+    	x = 1 ;
+	}
+}
+    // inclusao na area de indices
+
+		e1[cont].cod = cod;
+		cout << "RA do aluno: ";
+		cin >> codAluno;
+		e1[cont].codRa = codAluno;
+		cout << "Nome: " << buscarAlunos(a1,ia1,contA,codAluno) << endl;
+		cout << "Código do livro: ";
+		cin >> codLivro; 
+		e1[cont].codLivro = codLivro;
+		cout << "Descrição: " << buscarLivros(l1,il1,contL,codLivro) << endl;
+		cout << "Data do empréstimo" << endl;
+		cout << "Dia: ";
+		cin >> e1[cont].diaEmp;
+		cout << "Mês:";
+		cin >> e1[cont].mesEmp;
+		cout << "Ano: ";
+		cin >> e1[cont].anoEmp;
+		cout << "Data de devolução" << endl;
+		cout << "Dia: ";
+		cin >> e1[cont].diaDev;
+		cout << "Mês:";
+		cin >> e1[cont].mesDev;
+		cout << "Ano: ";
+		cin >> e1[cont].anoDev;
+		strcpy(e1[cont].statusEmp,"E");
+		e1[cont].status=0;
+    
+    if(cont == 0){
+	i1[0].cod = cod;
+    i1[0].end = cont;
+	}
+	else{
+    for (i = cont - 1; i1[i].cod > cod && i >= 0; i--){
+        i1[i+1].cod = i1[i].cod;
+        i1[i+1].end = i1[i].end;
+    }
+    i1[i+1].cod = cod;
+    i1[i+1].end = cont;
+}
+    cout << "\n\nInclusao realizada com Sucesso";
+    cont++;
+    getch();
+
+}
+
+void devolverLivros(struct Emprestimos e1[],struct IdxEmprestimos i1[], int &cont, struct Livros l1[], struct IdxLivros il1[], int contL, struct Alunos a1[], struct IdxAlunos ia1[], int contA){
+	
+}
+
+void excluirEmprestimo(struct Emprestimos e1[], struct IdxEmprestimos idx[], int cont, int cod){
+    int i = 0, f = cont;
+    int m = (i + f) / 2;
+    for (; f >= i && cod != idx[m].cod; m = (i + f) / 2){
+        if (cod > idx[m].cod)
+            i = m + 1;
+        else
+            f = m - 1;
+    }
+    i = idx[m].end;
+    if ((cod == idx[m].cod) && e1[i].status == 0){
+        e1[i].status = 1;
+        cout << "\n\n Empréstimo excluido com Sucesso";
+    }
+    else
+        cout << "Empréstimo não cadastrado";
+}
+
+void mostrarAtrasado(struct Emprestimos e1[],struct IdxEmprestimos i1[], int &cont, struct Livros l1[], struct IdxLivros il1[], int contL, struct Alunos a1[], struct IdxAlunos ia1[], int contA){
 }
 
 void menuAlunos (struct Alunos a1[], struct IdxAlunos i1[], int &cont){
@@ -373,13 +601,13 @@ void menuAlunos (struct Alunos a1[], struct IdxAlunos i1[], int &cont){
 			break;
 		case 2:
 			cout << "\n\nInclusão de Aluno";
-			cout << "RA: ";
+			cout << "\nRA: ";
     		cin >> cod;
 			incluirAlunos(a1,i1,cont,cod);
 			break;
 		case 3:
 			cout << "\n\nExclusão de Aluno";
-			cout << "Digite o código do aluno que deseja excluir: ";
+			cout << "\nDigite o código do aluno que deseja excluir: ";
     		cin >> cod;
 			excluirAlunos(i1,a1,cont,cod);
 			break;
@@ -413,19 +641,19 @@ void menuEditoras (struct Editoras e1[], struct IdxEditoras i1[], int &cont){
 			break;
 		case 2:
 			cout << "\n\nInclusão de Editora";
-			cout << "Codigo: ";
+			cout << "\nCodigo: ";
     		cin >> cod;
 			incluirEditoras(e1,i1,cont,cod);
 			break;
 		case 3:
 			cout << "\n\nExclusão de Editora";
-			cout << "Digite o código da editora que deseja excluir: ";
+			cout << "\nDigite o código da editora que deseja excluir: ";
     		cin >> cod;
 			excluirEditoras(i1,e1,cont,cod);
 			break;
-/*		case 4:
+		case 4:
 			mostrarEditoras(e1,i1,cont);
-			break;*/
+			break;
 		case 0:
 			x=1;
 			break;		
@@ -433,7 +661,7 @@ void menuEditoras (struct Editoras e1[], struct IdxEditoras i1[], int &cont){
 	}
 }
 
-void menuLivros (struct Livros l1[], struct IdxLivros i1[], int &cont){
+void menuLivros (struct Livros l1[], struct IdxLivros i1[], int &cont, struct Editoras e1[],struct IdxEditoras ie1[], int contEdit){
 	int option = 0, x = 0, cod = 0;
 	
 	while(x == 0){
@@ -449,23 +677,60 @@ void menuLivros (struct Livros l1[], struct IdxLivros i1[], int &cont){
 	
 	switch(option){
 		case 1:
-			leituraLivros(l1,i1,cont);
+			leituraLivros(l1,i1,cont,e1,ie1,contEdit);
 			break;
 		case 2:
 			cout << "\n\nInclusão de Livros";
-			cout << "Codigo: ";
+			cout << "\nCodigo: ";
     		cin >> cod;
-			incluirLivros(l1,i1,cont,cod);
+			incluirLivros(l1,i1,cont,cod,e1,ie1,contEdit);
 			break;
 		case 3:
 			cout << "\n\nExclusão de Livros";
-			cout << "Digite o código do Livro que deseja excluir: ";
+			cout << "\nDigite o código do Livro que deseja excluir: ";
     		cin >> cod;
 			excluirLivros(i1,l1,cont,cod);
 			break;
-/*		case 4:
-			mostrarLivros(l1,i1,cont);
-			break;*/
+		case 4:
+			mostrarLivros(l1,i1,cont,e1,ie1,contEdit);
+			break;
+		case 0:
+			x=1;
+			break;		
+		}
+	}
+}
+
+void menuEmprestimos (struct Emprestimos e1[],struct IdxEmprestimos i1[], int &cont, struct Livros l1[], struct IdxLivros il1[], int contL, struct Alunos a1[], struct IdxAlunos ia1[], int contA){
+	int option = 0, x = 0, cod = 0;
+	
+	while(x == 0){
+	system("cls");
+	cout << "\n\nEmpréstimos";
+	cout << "\n0 - Voltar";
+	cout << "\n1 - Emprestar Livro";
+	cout << "\n2 - Devolver Livro";
+	cout << "\n3 - Excluir Empréstimo";
+	cout << "\n4 - Mostrar Empréstimos Atrasados";
+	cout << "\n\nDigite sua opção: ";
+	cin >> option;
+	
+	switch(option){
+		case 1:
+			cout << "\n\nEmpréstimo de Livros";
+			cout << "\nCódigo: ";
+    		cin >> cod;
+			emprestarLivros(e1,i1,cont,cod,l1,il1,contL,a1,ia1,contA);
+			break;
+		case 2:
+			devolverLivros(e1,i1,cont,l1,il1,contL,a1,ia1,contA);
+			break;
+		case 3:
+			excluirEmprestimo(e1,i1,cont,cod);
+			break;
+		case 4:
+			mostrarAtrasado(e1,i1,cont,l1,il1,contL,a1,ia1,contA);
+			break;
 		case 0:
 			x=1;
 			break;		
@@ -474,9 +739,15 @@ void menuLivros (struct Livros l1[], struct IdxLivros i1[], int &cont){
 }
 
 int main (){
-	struct Alunos a1[20]; struct Editoras e1[20]; struct Livros l1[20];
-	struct IdxAlunos ia1[20]; struct IdxEditoras ie1[20]; struct IdxLivros il1[20];
-	int x = 1, option = 0, contA, contE, contL;
+	struct Alunos a1[20]; 
+	struct Editoras e1[20]; 
+	struct Livros l1[20];
+	struct Emprestimos emp1[20];
+	struct IdxAlunos ia1[20]; 
+	struct IdxEditoras ie1[20]; 
+	struct IdxLivros il1[20];
+	struct IdxEmprestimos iemp1[20];
+	int x = 1, option = 0, contA = 0, contE = 0, contL = 0, contEMP=0;
 	setlocale(LC_ALL,"Portuguese");
 	
 	while(x == 1){
@@ -490,13 +761,16 @@ int main (){
 				menuAlunos(a1,ia1,contA);
 				break;
 			case 2:
-				menuLivros(l1,il1,contL);
+				menuLivros(l1,il1,contL,e1,ie1,contE);
 				break;
 			case 3:
 				menuEditoras(e1,ie1,contE);
 				break;
+			case 4:
+				menuEmprestimos(emp1,iemp1,contEMP,l1,il1,contL,a1,ia1,contA);
+				break;
 			case 0:
-				x=1;
+				x=0;
 				break;
 		
 	}
